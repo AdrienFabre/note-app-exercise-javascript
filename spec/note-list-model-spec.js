c29e09e94b0a2aaeaa2ea042c556156e8092da19
AdrienFabre <adrien.fabre.1@gmail.com>
2019-02-18 18:57:00 +0000

    Read and create list of notes

---
+++ spec/note-list-model-spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function testReturnAllTheNotes() {
+    var noteList = new NoteList
+    noteList.list = ['My favourite language is Russian.', 'My favourite music is the sea.' ]
+    assert.isTrue(noteList.listAll() === ('My favourite language is Russian.', 'My favourite language is Russian.'))
+    console.log("testReturnAllTheNotes is passing");
+};
+testReturnAllTheNotes()
+
+function testCreatesAndStoresANewSingleNote() {
+    var noteList = new NoteList
+    noteList.createNote('Favourite place: jungle.')
+    assert.isTrue( noteList.list[0] === ('Favourite place: jungle.'))
+    console.log("testCreatesAndStoresANewSingleNote is passing");
+};
+testCreatesAndStoresANewSingleNote()
